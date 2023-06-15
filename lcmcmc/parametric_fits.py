8d5fc27dbadd4eb75d6ad35abe9ff65450d7b70a
b-biswas <bbiswas.dgp@gmail.com>
2023-06-15 12:37:23 +0200

    parametrics fit func for pcs

--- lcmcmc/parametric_fits.py
+++ lcmcmc/parametric_fits.py
@@ -9,3 +9,8 @@ def parametric_fn(t0, t_rise, t_fall, amplitude, x_range):
     denominator = 1 + jnp.exp(-time_dif/t_rise)
     predicted_lc = amplitude * ( numerator/ denominator )
     return predicted_lc
+
+def parametric_fn_pcs(c1, c2, c3 , pcs, positions):
+    index = positions
+    predicted_lc = pcs[0][index[:]]*c1 + pcs[1][index[:]]*c2 + pcs[2][index[:]]*c3
+    return predicted_lc
