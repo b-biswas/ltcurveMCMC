5245317a673367513da75895c89a3609dfa6a71a
b-biswas <bbiswas.dgp@gmail.com>
2023-08-07 16:19:14 +0200

    jit the predictions

--- lcmcmc/parametric_fits.py
+++ lcmcmc/parametric_fits.py
@@ -1,7 +1,9 @@
 #define the parametric function of the lightcurve
 import tensorflow_probability.substrates.jax as tfp
 import jax.numpy as jnp
+import jax
 
+@jax.jit
 def parametric_fn(t0, t_rise, t_fall, amplitude, x_range):
     
     time_dif = x_range - t0
@@ -10,7 +12,8 @@ def parametric_fn(t0, t_rise, t_fall, amplitude, x_range):
     predicted_lc = amplitude * ( numerator/ denominator )
     return predicted_lc
 
-def parametric_fn_pcs(c1, c2, c3 , pcs, positions):
+@jax.jit
+def parametric_fn_pcs(c1, c2, c3, pcs, positions):
     index = positions
     predicted_lc = pcs[0][index[:]]*c1 + pcs[1][index[:]]*c2 + pcs[2][index[:]]*c3
     return predicted_lc
